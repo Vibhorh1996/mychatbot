bff1807fc3de3c7755acea6258bd75fb440a60d4
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-22 14:24:24 +0530

    Updated code for FAISS

---
+++ multipleFile_Upload.py
@@ -0,0 +1,55 @@
+import os
+import streamlit as st
+import tiktoken
+import faiss
+import pypdf
+from langdetect import detect
+from iso639 import languages
+
+# Create FAISS index
+index = faiss.IndexFlatL2(768)
+
+# Function to tokenize PDF content
+def tokenize_pdf_content(pdf_file):
+    with open(pdf_file, 'rb') as file:
+        reader = pypdf.PdfReader(file)
+        content = ''
+        for page in reader.pages:
+            content += page.extract_text()
+        return tiktoken.tokenize(content)
+
+# Function to index PDF files
+def index_pdf_files(pdf_files):
+    documents = []
+    for pdf_file in pdf_files:
+        tokens = tokenize_pdf_content(pdf_file)
+        documents.append(tokens)
+    doc_tensors = [faiss.tensor_from_array(doc, dim=1) for doc in documents]
+    doc_ids = faiss.IndexIDMap(index)
+    doc_ids.add_with_ids(faiss.cat(doc_tensors), np.arange(len(doc_tensors)))
+
+# Function to search for query in the PDFs
+def search_pdf(query):
+    query_tokens = tiktoken.tokenize(query)
+    query_tensor = faiss.tensor_from_array(query_tokens, dim=1)
+    _, I = index.search(query_tensor, k=5)  # Search top 5 most similar documents
+    results = []
+    for i in I[0]:
+        results.append(pdf_files[i])
+    return results
+
+# Streamlit app
+st.title("PDF Search")
+pdf_files = st.file_uploader("Upload PDF files", accept_multiple_files=True)
+if pdf_files:
+    index_pdf_files(pdf_files)
+
+query = st.text_input("Enter your query:")
+if query:
+    results = search_pdf(query)
+    if results:
+        st.write("Matching PDF files:")
+        for result in results:
+            st.write(result)
+    else:
+        st.write("No matching PDF files found.")
