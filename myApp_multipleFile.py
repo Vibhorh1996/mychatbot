fe1d90152eac5a6960fdc4c4ee6e95535e0e4ab1
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-24 18:37:23 +0530

    Update myApp_multipleFile.py

--- myApp_multipleFile.py
+++ myApp_multipleFile.py
@@ -226,8 +226,8 @@ uploaded_files = st.file_uploader("Choose one or more PDF files", type="pdf", ac
 
 if uploaded_files: # if there are uploaded files
     # initialize OpenAI embeddings and chat model
-    embeddings = OpenAIEmbeddings(openai_api_key=key)
-    chat = ChatOpenAI(temperature=0, openai_api_key=key)
+    embeddings = OpenAIEmbeddings(openai_api_key=key, model_name = model)
+    chat = ChatOpenAI(temperature=0, openai_api_key=key, model_name = model)
 
     faiss_obj_path = "models/all_files.pickle" # define the path to save or load the FAISS object for all files
     file_paths = [] # a list to store all the file paths of uploaded files
