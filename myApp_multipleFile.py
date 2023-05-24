7ce713bfa2c0c0ce5669107842a284b26d100b0a
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-24 21:16:16 +0530

    Update myApp_multipleFile.py

--- myApp_multipleFile.py
+++ myApp_multipleFile.py
@@ -29,6 +29,7 @@ st.markdown("<h2 stype='text-align:center;'>A Chatbot for conversing with your d
 
 # set API Key
 key = st.text_input('OpenAI API Key','',type='password')
+openai.api_key = key
 os.environ['OPENAI_API_KEY'] = key
 
 # initialize session state variables
