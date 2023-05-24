66bd7db300ce561b83e60a83199ea4d84c50601a
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-24 17:45:24 +0530

    Add files via upload

---
+++ src/openai_embeddings.py
@@ -0,0 +1,27 @@
+import openai
+
+class OpenAIEmbeddings:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.tokenizer = openai.Tokenizer(model=model_name)
+        self.encoder = openai.Encoder(model=model_name)
+    
+    def embed(self, text):
+        inputs = self.tokenizer.encode(text)
+        embedding = self.encoder.encode(inputs)
+        return embedding
+
+
+class ChatOpenAI:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.chat_model = openai.ChatCompletion.create(model=model_name)
+    
+    def generate_response(self, input_text):
+        response = self.chat_model.create(
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": input_text}
+            ]
+        )
+        return response.choices[0].message.content
