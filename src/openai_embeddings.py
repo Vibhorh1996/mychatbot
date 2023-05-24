769591c39a07708add99caa9a9779f5bfbbaf0e6
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-24 20:38:16 +0530

    Update openai_embeddings.py

--- src/openai_embeddings.py
+++ src/openai_embeddings.py
@@ -5,7 +5,7 @@ class OpenAIEmbeddings:
     def __init__(self, model_name, openai_api_key):
         self.model_name = model_name
         self.openai_api_key = openai_api_key
-        self.tokenizer = openai.Tokenizer(model=model_name)
+        self.tokenizer = Tokenizer(model=model_name)
         self.encoder = openai.Encoder(model=model_name)
     
     def embed(self, text):
