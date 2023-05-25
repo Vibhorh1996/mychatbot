f5b14ed768901ab376e95b2982dba3be848e0b82
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-25 13:03:22 +0530

    Update embeddings.py

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,22 +1,22 @@
 import openai
 
 class Embed:
-    def __init__(self, model, inputs, engine="davinci-codex", prompt_label="text"):
-        self.model = model
+    def __init__(self, inputs, engine="davinci-codex", prompt_label="text"):
+        #self.model = model
         self.inputs = inputs
         self.engine = engine
         self.prompt_label = prompt_label
         self.embeddings = None
 
     @classmethod
-    def create(cls, model, inputs, engine="davinci-codex", prompt_label="text"):
-        embed = cls(model, inputs, engine, prompt_label)
+    def create(cls, inputs, engine="davinci-codex", prompt_label="text"):
+        embed = cls(inputs, engine, prompt_label)
         embed.create_embeddings()
         return embed
 
     def create_embeddings(self):
         response = openai.Completion.create(
-            model=self.model,
+            #model=self.model removed model param from here and from all other functions where it was taken as a param. init,create
             inputs=self.inputs,
             engine=self.engine,
             prompt_label=self.prompt_label,
