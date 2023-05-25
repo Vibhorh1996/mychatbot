26a1bbeb8000b68f4962d85267a035792efc390a
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-25 12:12:51 +0530

    Update openai_embeddings.py

--- src/openai_embeddings.py
+++ src/openai_embeddings.py
@@ -12,9 +12,7 @@ class OpenAIEmbeddings:
             self.chat_model = openai.ChatCompletion.create(model=model_name, messages = messages)
         self.tokenizer = openai.ChatCompletion.create(model=model_name, messages=self.messages)
         #self.encoder = openai.TextEmbeddings.create(model=model_name)
-             @retry(
-                stop=stop_after_attempt(3),  # Retry for a maximum of 3 attempts
-                wait=wait_exponential(multiplier=1, min=2, max=5)  # Exponential backoff with a base of 2
+             
     def embed(self, text):
         inputs = self.tokenizer.encode(text)
         embedding = openai.Embed.create(
@@ -24,11 +22,6 @@ class OpenAIEmbeddings:
             prompt_label="text"
         )
         return embedding.choices[0].doc_embeddings
-    
-#     def embed(self, text):
-#         inputs = self.tokenizer.encode(text)
-#         embedding = openai.Embed(inputs)
-#         return embedding
 
 
 class ChatOpenAI:
@@ -41,23 +34,9 @@ class ChatOpenAI:
         self.chat_model = openai.ChatCompletion.create(model=model_name, messages=messages)
     
     
-   
-#     def generate_response(self, input_text):
-#         self.messages.append({"role": "system", "content": "You are a helpful assistant."})
-#         self.messages.append({"role": "user", "content": input_text})
-        
-#         while True:
-#             try:
-#                 response = self.chat_model.create(messages=self.messages)
-#                 break  # Break out of the loop if the request succeeds
-#             except openai.error.RateLimitError as e:
-#                 print("Rate limit reached. Waiting for 20 seconds...")
-#                 time.sleep(20)  # Wait for 20 seconds
-#                 continue  # Retry the request
-            
-#         self.messages = [{"role": "system", "content": response.choices[0].message.content}]
-#         return response.choices[0].message.content
-    
+    @retry(
+                stop=stop_after_attempt(3),  # Retry for a maximum of 3 attempts
+                wait=wait_exponential(multiplier=1, min=2, max=5)  # Exponential backoff with a base of 2
     def generate_response(self, input_text):
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
@@ -66,21 +45,3 @@ class ChatOpenAI:
         response = self.chat_model.create(messages=messages)
         return response.choices[0].message.content
 
-    
-#     def generate_response(self, input_text):
-#         messages = [
-#             {"role": "system", "content": "You are a helpful assistant."},
-#             {"role": "user", "content": input_text}
-#         ]
-#         response = self.chat_model.create(messages=messages)
-#         return response.choices[0].message.content
-
-    
-#     def generate_response(self, input_text):
-#         response = self.chat_model.create(
-#             messages=[
-#                 {"role": "system", "content": "You are a helpful assistant."},
-#                 {"role": "user", "content": input_text}
-#             ]
-#         )
-#         return response.choices[0].message.content
