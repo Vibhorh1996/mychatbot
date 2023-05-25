0a16373628fa65acc7c0b858ec2dcc5827d8c4fb
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-25 17:23:01 +0530

    Update embeddings.py

--- src/embeddings.py
+++ src/embeddings.py
@@ -8,22 +8,22 @@ class Embed:
         self.prompt_label = prompt_label
         self.embeddings = None
 
-    @classmethod
-    def create(cls, model, inputs,  engine="davinci-codex", prompt_label="text"): #engine="davinci-codex",
-        embed = cls(inputs, engine, prompt_label) #engine,
-        embed.create_embeddings()
-        return embed
-
     def create_embeddings(self):
         response = openai.Completion.create(
             model=self.model,
             inputs=self.inputs,
             engine=self.engine, #removed engine param from here and from all other functions where it was taken as a param. init,create
             prompt_label=self.prompt_label,
-            Embed=True
+            embed=True
         )
         print(response)
         self.embeddings = response.choices[0].doc_embeddings
+    
+    @classmethod
+    def create(cls, model, inputs,  engine="davinci-codex", prompt_label="text"): #engine="davinci-codex",
+        embed = cls(inputs, engine, prompt_label) #engine,
+        embed.create_embeddings()
+        return embed
 
     def get_embeddings(self):
         return self.embeddings
