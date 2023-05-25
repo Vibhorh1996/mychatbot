c301d98024592924dc19fd3982112f8e5a2e0312
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-25 12:18:47 +0530

    Update openai_embeddings.py

--- src/openai_embeddings.py
+++ src/openai_embeddings.py
@@ -23,20 +23,19 @@ class OpenAIEmbeddings:
         )
         return embedding.choices[0].doc_embeddings
 
-
 class ChatOpenAI:
     def __init__(self, model_name, openai_api_key, messages=None):
         self.model_name = model_name
         self.openai_api_key = openai_api_key
         if messages is None:
             messages = [{"role": "system", "content": "You are a helpful assistant."}]
-            self.chat_model = openai.ChatCompletion.create(model=model_name, messages = messages)
+            self.chat_model = openai.ChatCompletion.create(model=model_name, messages=messages)
         self.chat_model = openai.ChatCompletion.create(model=model_name, messages=messages)
     
-    
     @retry(
-                stop=stop_after_attempt(3),  # Retry for a maximum of 3 attempts
-                wait=wait_exponential(multiplier=1, min=2, max=5)  # Exponential backoff with a base of 2
+        stop=stop_after_attempt(3),  # Retry for a maximum of 3 attempts
+        wait=wait_exponential(multiplier=1, min=2, max=5)  # Exponential backoff with a base of 2
+    )
     def generate_response(self, input_text):
         messages = [
             {"role": "system", "content": "You are a helpful assistant."},
@@ -44,4 +43,25 @@ class ChatOpenAI:
         ]
         response = self.chat_model.create(messages=messages)
         return response.choices[0].message.content
+    
+# class ChatOpenAI:
+#     def __init__(self, model_name, openai_api_key, messages=None):
+#         self.model_name = model_name
+#         self.openai_api_key = openai_api_key
+#         if messages is None:
+#             messages = [{"role": "system", "content": "You are a helpful assistant."}]
+#             self.chat_model = openai.ChatCompletion.create(model=model_name, messages = messages)
+#         self.chat_model = openai.ChatCompletion.create(model=model_name, messages=messages)
+    
+    
+#     @retry(
+#                 stop=stop_after_attempt(3),  # Retry for a maximum of 3 attempts
+#                 wait=wait_exponential(multiplier=1, min=2, max=5)  # Exponential backoff with a base of 2
+#     def generate_response(self, input_text):
+#         messages = [
+#             {"role": "system", "content": "You are a helpful assistant."},
+#             {"role": "user", "content": input_text}
+#         ]
+#         response = self.chat_model.create(messages=messages)
+#         return response.choices[0].message.content
 
