b33fccb8e7851533f7a8e4f1562f3a4f1858f393
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-25 12:05:56 +0530

    Update openai_embeddings.py

--- src/openai_embeddings.py
+++ src/openai_embeddings.py
@@ -1,4 +1,5 @@
 import openai
+from tenacity import retry, stop_after_attempt, wait_exponential
 from tokenizer.tokenizer import Tokenizer
 
 class OpenAIEmbeddings:
@@ -11,7 +12,9 @@ class OpenAIEmbeddings:
             self.chat_model = openai.ChatCompletion.create(model=model_name, messages = messages)
         self.tokenizer = openai.ChatCompletion.create(model=model_name, messages=self.messages)
         #self.encoder = openai.TextEmbeddings.create(model=model_name)
-    
+     @retry(
+        stop=stop_after_attempt(3),  # Retry for a maximum of 3 attempts
+        wait=wait_exponential(multiplier=1, min=2, max=5)  # Exponential backoff with a base of 2
     def embed(self, text):
         inputs = self.tokenizer.encode(text)
         embedding = openai.Embed.create(
@@ -36,30 +39,32 @@ class ChatOpenAI:
             messages = [{"role": "system", "content": "You are a helpful assistant."}]
             self.chat_model = openai.ChatCompletion.create(model=model_name, messages = messages)
         self.chat_model = openai.ChatCompletion.create(model=model_name, messages=messages)
-   
-    def generate_response(self, input_text):
-        self.messages.append({"role": "system", "content": "You are a helpful assistant."})
-        self.messages.append({"role": "user", "content": input_text})
-        
-        while True:
-            try:
-                response = self.chat_model.create(messages=self.messages)
-                break  # Break out of the loop if the request succeeds
-            except openai.error.RateLimitError as e:
-                print("Rate limit reached. Waiting for 20 seconds...")
-                time.sleep(20)  # Wait for 20 seconds
-                continue  # Retry the request
-            
-        self.messages = [{"role": "system", "content": response.choices[0].message.content}]
-        return response.choices[0].message.content
     
+    
+   
 #     def generate_response(self, input_text):
-#         messages = [
-#             {"role": "system", "content": "You are a helpful assistant."},
-#             {"role": "user", "content": input_text}
-#         ]
-#         response = self.chat_model.create(messages=messages)
+#         self.messages.append({"role": "system", "content": "You are a helpful assistant."})
+#         self.messages.append({"role": "user", "content": input_text})
+        
+#         while True:
+#             try:
+#                 response = self.chat_model.create(messages=self.messages)
+#                 break  # Break out of the loop if the request succeeds
+#             except openai.error.RateLimitError as e:
+#                 print("Rate limit reached. Waiting for 20 seconds...")
+#                 time.sleep(20)  # Wait for 20 seconds
+#                 continue  # Retry the request
+            
+#         self.messages = [{"role": "system", "content": response.choices[0].message.content}]
 #         return response.choices[0].message.content
+    
+    def generate_response(self, input_text):
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": input_text}
+        ]
+        response = self.chat_model.create(messages=messages)
+        return response.choices[0].message.content
 
     
 #     def generate_response(self, input_text):
