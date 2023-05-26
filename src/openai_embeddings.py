47206665ec8ed7cf9cb1fb2ace74ed7a1fc8840a
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-26 09:24:07 +0530

    Update openai_embeddings.py

--- src/openai_embeddings.py
+++ src/openai_embeddings.py
@@ -43,13 +43,13 @@ class ChatOpenAI:
         wait=wait_exponential(multiplier=1, min=2, max=5)
     )
     def generate_response(self, input_text):
-    response = self.chat_model.create(
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": input_text}
-        ]
-    )
-    return response.choices[0].message.content
+        response = self.chat_model.create(
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": input_text}
+            ]
+        )
+        return response.choices[0].message.content
     
 #     def generate_response(self, input_text):
 #         messages = self.messages + [
