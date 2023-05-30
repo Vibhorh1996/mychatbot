bfcc54111037a746602ddc9b1ce0c0583233300a
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-30 11:17:30 +0530

    Update myApp_old.py

--- myApp_old.py
+++ myApp_old.py
@@ -181,7 +181,7 @@ def answer_questions(faiss_index, user_input):
 openai = OpenAI()
 
 # create an OpenAIEmbeddings object
-embeddings = OpenAIEmbeddings(openai)
+embeddings = OpenAIEmbeddings(model)
 
 # create a ChatOpenAI object
 chat = ChatOpenAI(openai, model)
