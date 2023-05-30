cb8f051fd42e2aac436d911599d1580bcfbae85a
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-30 16:00:03 +0530

    Update myApp_old.py

--- myApp_old.py
+++ myApp_old.py
@@ -173,7 +173,7 @@ vectorizer = TfidfVectorizer()
 
 def vectorize(text, document_content):
     # Fit the vectorizer on the document content
-    vectorizer.fit(document_content.text)
+    vectorizer.fit([doc.text for doc in document_content]) # use a list comprehension to get the text of each document
 
     # Transform the text into a TF-IDF vector
     vector = vectorizer.transform([text])
