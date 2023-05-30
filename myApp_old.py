89605cde5283b179dd26054a94f131937460a58d
Vibhorh1996 <vibhorh1996@gmail.com>
2023-05-30 16:17:13 +0530

    Update myApp_old.py

--- myApp_old.py
+++ myApp_old.py
@@ -15,7 +15,7 @@ import mimetypes
 import time
 from bs4 import BeautifulSoup
 import tiktoken
-import PyPDF2
+from PyPDF2 import *
 from scipy.spatial.distance import cosine
 from sklearn.feature_extraction.text import TfidfVectorizer
 from urllib.parse import urljoin, urlsplit
