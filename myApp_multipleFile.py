52856ebb6ed57c31a344337fac537b153932d7bc
Vibhorh1996 <vibhorh1996@gmail.com>
2023-06-01 20:26:58 +0530

    Update myApp_multipleFile.py

--- myApp_multipleFile.py
+++ myApp_multipleFile.py
@@ -1,113 +1,171 @@
+# Importing the required libraries
 import streamlit as st
-import requests
-from bs4 import BeautifulSoup
 import pandas as pd
+import requests
 import pickle
-import os
-import re
-import json
-import openai
-import pypdf2
+import PyPDF2
+from bs4 import BeautifulSoup
+from openai import api_key
 
-# Set your OpenAI API key here
-#OPENAI_API_KEY = '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'
+# Setting the OpenAI API key
+api_key = st.text_input("Please enter your OpenAI API key")
+openai.api_key = api_key
 
-def generate_response(input_text, model='gpt-3.5-turbo', max_tokens=50):
-    api = OpenAIApi(OPENAI_API_KEY)
-    response = api.complete(
-        prompt=input_text,
-        model=model,
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=0.6,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0,
-        log_level='info'
-    )
-    return response['choices'][0]['text'].strip()
+# Creating a list of available language models
+models = ["ada", "babbage", "curie", "davinci"]
 
-def run_data_chat():
-    # Set Streamlit app title
-    st.title("Data Chat")
+# Creating a dropdown menu for model selection
+model = st.selectbox("Please select a language model", models)
 
-    # Display app description and instructions
-    st.markdown("Data Chat is a chatbot application that allows users to interact with a pre-trained language model.")
-    st.markdown("To get started, please provide your OpenAI API key:")
+# Creating a file uploader for PDF files
+uploaded_files = st.file_uploader("Please upload PDF files", type="pdf", accept_multiple_files=True)
 
-    # Get OpenAI API key from user input
-    openai_api_key = st.text_input("OpenAI API Key")
+# Creating an empty list to store the PDF text
+pdf_text = []
 
-    # Set OpenAI API key in environment variable
-    os.environ["OPENAI_API_KEY"] = openai_api_key
+# Parsing the PDF files and extracting the text
+if uploaded_files:
+    for file in uploaded_files:
+        pdf_reader = PyPDF2.PdfFileReader(file)
+        num_pages = pdf_reader.numPages
+        for page in range(num_pages):
+            page_obj = pdf_reader.getPage(page)
+            text = page_obj.extractText()
+            pdf_text.append(text)
 
-    # Check if API key is provided
-    if not openai_api_key:
-        st.warning("Please provide your OpenAI API key to proceed.")
+# Creating a text input for user query
+query = st.text_input("Please enter your query")
+
+# Creating an empty string to store the response
+response = ""
+
+# Creating a variable to store the total cost of the conversation
+total_cost = 0
+
+# Defining a function to generate responses using the OpenAI API
+def generate_response(query, model):
+    global response
+    global total_cost
+
+    # Pre-processing the query text
+    query = query.lower()
+    query = query.strip()
+
+    # Checking if the query is empty or not
+    if query == "":
+        response = "Please enter a valid query"
         return
 
-    # Language model selection
-    models = {
-        'gpt-3.5-turbo': 'GPT-3.5 Turbo',
-        # Add more models here if desired
-    }
+    # Creating the prompt text by concatenating the PDF text and the instructions
+    prompt = "\n".join(pdf_text)
+    prompt += "\n\n'''"
+    prompt += "\nI want you to act as a documents that I am having a conversation with. Your name is 'AI Assistant'. You will provide me with answers from the given info. If the answer is not included, say exactly 'Hmm, I am not sure.' and stop after that. Refuse to answer any question not about the info. Never break character."
+    prompt += "\n'''"
 
-    selected_model = st.selectbox("Select a language model", list(models.values()))
+    # Appending the user query to the prompt text
+    prompt += "\n\nUser: " + query + "\nAI Assistant:"
 
-    # Set the chosen model for generating responses
-    model = next(key for key, value in models.items() if value == selected_model)
+    # Making an API request to generate a response
+    result = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=100,
+        stop="\n",
+        temperature=0.5,
+        frequency_penalty=0.5,
+        presence_penalty=0.5,
+        logprobs=10,
+        echo=False,
+        return_metadata=True,
+    )
 
-    # Start the conversation with the AI assistant
-    st.markdown("## Start a conversation with AI Assistant")
+    # Extracting the response text from the result object
+    response = result["choices"][0]["text"]
 
-    conversation_history = []
+    # Extracting the cost of the response from the result object
+    cost = result["metadata"]["cost"]
 
-    while True:
-        user_input = st.text_input("User:", key='user_input')
+    # Updating the total cost of the conversation
+    total_cost += cost
 
-        if st.button("Ask"):
-            # Add user input to conversation history
-            conversation_history.append({'role': 'user', 'content': user_input})
+# Calling the generate_response function with the user query and model as arguments
+generate_response(query, model)
 
-            # Generate response from the AI assistant
-            response = generate_response('\n'.join([f"{entry['role']}: {entry['content']}" for entry in conversation_history]),
-                                         model=model)
+# Displaying the response to the user
+st.write("AI Assistant:", response)
 
-            # Add AI assistant response to conversation history
-            conversation_history.append({'role': 'assistant', 'content': response})
+# Displaying the total cost of the conversation
+st.write("Total cost of this conversation:", total_cost)
+# Creating a text input for web scraping
+url = st.text_input("Please enter a URL to scrape information from")
 
-            # Display AI assistant response
-            st.text_area("AI Assistant:", value=response, height=100, key='response')
+# Creating an empty string to store the scraped content
+scraped_content = ""
 
-        if st.button("Reset Conversation"):
-            # Clear conversation history
-            conversation_history = []
+# Defining a function to scrape information from a web page
+def scrape_info(url):
+    global scraped_content
 
-        if st.button("End Conversation"):
-            # Clear conversation history and display conversation cost
-            conversation_tokens = sum([len(entry['content'].split()) for entry in conversation_history])
-            st.info(f"Conversation Tokens: {conversation_tokens}")
-            st.info(f"Conversation Cost: {conversation_tokens * 0.004:.2f} USD")
+    # Checking if the URL is empty or not
+    if url == "":
+        scraped_content = "Please enter a valid URL"
+        return
 
-            # Clear conversation history
-            conversation_history = []
+    # Making an HTTP GET request to the URL
+    response = requests.get(url)
 
-def upload_files():
-    uploaded_files = st.file_uploader('Upload PDF Files', type='pdf', accept_multiple_files=True)
-    return uploaded_files
-uploaded_files = upload_files()
+    # Checking if the response status code is 200 or not
+    if response.status_code != 200:
+        scraped_content = "The URL is not accessible"
+        return
 
-def parse_pdf(file):
-    pdf_reader = PyPDF2.PdfReader(file)
-    text = ''
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-    return text
-parsed_texts = []
-for file in uploaded_files:
-    text = parse_pdf(file)
-    parsed_texts.append(text)
-            
-if __name__ == '__main__':
-    run_data_chat()
+    # Parsing the HTML content of the response using BeautifulSoup
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Extracting the title of the web page
+    title = soup.find("title").text
+
+    # Extracting the text content of the web page
+    text = soup.get_text()
+
+    # Concatenating the title and text content
+    scraped_content = title + "\n" + text
+
+# Calling the scrape_info function with the URL as argument
+scrape_info(url)
+
+# Displaying the scraped content to the user
+st.write("Scraped content:", scraped_content)
+
+# Creating a text input for token counting
+text = st.text_input("Please enter a text string to count tokens")
+
+# Creating a variable to store the number of tokens
+num_tokens = 0
+
+# Defining a function to count tokens using a language model
+def count_tokens(text, model):
+    global num_tokens
+
+    # Checking if the text is empty or not
+    if text == "":
+        num_tokens = 0
+        return
+
+    # Making an API request to encode the text using a language model
+    result = openai.Encodings.create(
+        engine=model,
+        query=text,
+        max_tokens=100,
+        echo=False,
+        return_metadata=True,
+    )
+
+    # Extracting the number of tokens from the result object
+    num_tokens = result["metadata"]["tokens"]
+
+# Calling the count_tokens function with the text and model as arguments
+count_tokens(text, model)
+
+# Displaying the number of tokens to the user
+st.write("Number of tokens:", num_tokens)
